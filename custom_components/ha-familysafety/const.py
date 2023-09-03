60a25d88d1e097f3097275886ca48d49801a6143
Jordan H <jordan@hrvy.uk>
2023-09-03 22:50:52 +0000

    family_safety

--- custom_components/ha-familysafety/const.py
+++ custom_components/ha-familysafety/const.py
@@ -3,7 +3,6 @@ from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
+NAME = "Microsoft Family Safety"
+DOMAIN = "family_safety"
 VERSION = "0.0.0"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
