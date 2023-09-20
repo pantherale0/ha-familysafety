04645b9f9b162fcc784eee166cb87bb6895a3257
Jordan H <jordan@hrvy.uk>
2023-09-20 08:19:41 +0000

    1.1.0b2

--- custom_components/family_safety/const.py
+++ custom_components/family_safety/const.py
@@ -6,7 +6,7 @@ LOGGER: Logger = getLogger(__package__)
 
 NAME = "Microsoft Family Safety"
 DOMAIN = "family_safety"
-VERSION = "1.1.0b0"
+VERSION = "1.1.0b2"
 
 DEFAULT_OVERRIDE_ENTITIES = [OverrideTarget.MOBILE,
                              OverrideTarget.WINDOWS,
