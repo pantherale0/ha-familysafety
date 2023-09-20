c3eeeb491622b5a5ea96be21ac01b4df0c3fac32
Jordan H <jordan@hrvy.uk>
2023-09-20 08:14:33 +0000

    fix AttributeError

--- custom_components/family_safety/__init__.py
+++ custom_components/family_safety/__init__.py
@@ -54,7 +54,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     _LOGGER.debug("Unloading config entry %s", entry.entry_id)
-    hass.data[DOMAIN][entry.entry_id].api.end_session()
+    hass.data[DOMAIN][entry.entry_id].api.api.end_session()
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
 
