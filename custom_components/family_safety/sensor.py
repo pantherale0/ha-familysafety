7b9c172940298320cc54c5c2829e312df3b2b7d8
Jordan H <jordan@hrvy.uk>
2024-02-24 23:39:43 +0000

    1.2.0
    fix Authentication doesn't work #35
    resolve timezone issues preventing correct screen time values (#34)
    update translations
    bump pyfamilysafety to 0.3.0
    add account name to entity names

--- custom_components/family_safety/sensor.py
+++ custom_components/family_safety/sensor.py
@@ -11,42 +11,44 @@ from pyfamilysafety import Account
 from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback, async_get_current_platform
+from homeassistant.helpers.entity_platform import (
+    AddEntitiesCallback,
+    async_get_current_platform,
+)
 
 from .coordinator import FamilySafetyCoordinator
 
-from .const import (
-    DOMAIN
-)
+from .const import DOMAIN
 
 from .entity_base import ManagedAccountEntity, ApplicationEntity
 
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_setup_entry(
-        hass: HomeAssistant,
-        config_entry: ConfigEntry,
-        async_add_entities: AddEntitiesCallback
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up Family Safety sensors."""
     accounts: list[Account] = hass.data[DOMAIN][config_entry.entry_id].api.accounts
     entities = []
     for account in accounts:
         if (account.user_id in config_entry.options.get("accounts", [])) or (
-            len(config_entry.options.get("accounts", []))==0
+            len(config_entry.options.get("accounts", [])) == 0
         ):
             entities.append(
                 AccountScreentimeSensor(
                     coordinator=hass.data[DOMAIN][config_entry.entry_id],
                     idx=None,
-                    account_id=account.user_id
+                    account_id=account.user_id,
                 )
             )
             entities.append(
                 AccountBalanceSensor(
                     coordinator=hass.data[DOMAIN][config_entry.entry_id],
                     idx=None,
-                    account_id=account.user_id
+                    account_id=account.user_id,
                 )
             )
             for app in config_entry.options.get("tracked_applications", []):
@@ -55,7 +57,7 @@ async def async_setup_entry(
                         coordinator=hass.data[DOMAIN][config_entry.entry_id],
                         idx=None,
                         account_id=account.user_id,
-                        app_id=app
+                        app_id=app,
                     )
                 )
 
@@ -64,18 +66,15 @@ async def async_setup_entry(
     platform = async_get_current_platform()
     platform.async_register_entity_service(
         name="block_app",
-        schema={
-            vol.Required("name"): str
-        },
-        func="async_block_application"
+        schema={vol.Required("name"): str},
+        func="async_block_application",
     )
     platform.async_register_entity_service(
         name="unblock_app",
-        schema={
-            vol.Required("name"): str
-        },
-        func="async_unblock_application"
+        schema={vol.Required("name"): str},
+        func="async_unblock_application",
     )
+
 
 class AccountBalanceSensor(ManagedAccountEntity, SensorEntity):
     """A balance sensor for the account."""
@@ -87,7 +86,7 @@ class AccountBalanceSensor(ManagedAccountEntity, SensorEntity):
     @property
     def name(self) -> str:
         """Return name of entity."""
-        return "Available Balance"
+        return f"{self._account.first_name} Available Balance"
 
     @property
     def native_value(self) -> float:
@@ -104,6 +103,7 @@ class AccountBalanceSensor(ManagedAccountEntity, SensorEntity):
         """Return device class."""
         return SensorDeviceClass.MONETARY
 
+
 class AccountScreentimeSensor(ManagedAccountEntity, SensorEntity):
     """Aggregate screentime sensor."""
 
@@ -114,12 +114,12 @@ class AccountScreentimeSensor(ManagedAccountEntity, SensorEntity):
     @property
     def name(self) -> str:
         """Return entity name."""
-        return "Used Screen Time"
+        return f"{self._account.first_name} Used Screen Time"
 
     @property
     def native_value(self) -> float:
         """Return duration (minutes)."""
-        return (self._account.today_screentime_usage/1000)/60
+        return (self._account.today_screentime_usage / 1000) / 60
 
     @property
     def native_unit_of_measurement(self) -> str | None:
@@ -137,16 +137,14 @@ class AccountScreentimeSensor(ManagedAccountEntity, SensorEntity):
         devices = {}
         for device in self._account.devices:
             if device.today_time_used:
-                devices[device.device_name] = (device.today_time_used/1000)/60
+                devices[device.device_name] = (device.today_time_used / 1000) / 60
             else:
                 devices[device.device_name] = 0
         applications = {}
         for app in self._account.applications:
             applications[app.name] = app.usage
-        return {
-            "application_usage": applications,
-            "device_usage": devices
-        }
+        return {"application_usage": applications, "device_usage": devices}
+
 
 class ApplicationScreentimeSensor(ApplicationEntity, SensorEntity):
     """Application specific screentime sensor."""
@@ -154,7 +152,7 @@ class ApplicationScreentimeSensor(ApplicationEntity, SensorEntity):
     @property
     def name(self) -> str:
         """Return entity name."""
-        return f"{self._application.name} Used Screen Time"
+        return f"{self._account.first_name} {self._application.name} Used Screen Time"
 
     @property
     def native_value(self) -> float:
@@ -174,6 +172,4 @@ class ApplicationScreentimeSensor(ApplicationEntity, SensorEntity):
     @property
     def extra_state_attributes(self) -> Mapping[str, Any] | None:
         """Return additional state attributes."""
-        return {
-            "blocked": self._application.blocked
-        }
+        return {"blocked": self._application.blocked}
--- custom_components/family_safety/switch.py
+++ custom_components/family_safety/switch.py
@@ -12,27 +12,26 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .coordinator import FamilySafetyCoordinator
 
-from .const import (
-    DOMAIN,
-    DEFAULT_OVERRIDE_ENTITIES
-)
+from .const import DOMAIN, DEFAULT_OVERRIDE_ENTITIES
 
 from .entity_base import PlatformOverrideEntity, ApplicationEntity
 
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_setup_entry(
-        hass: HomeAssistant,
-        config_entry: ConfigEntry,
-        async_add_entities: AddEntitiesCallback
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up Family Safety switches."""
     accounts: list[Account] = hass.data[DOMAIN][config_entry.entry_id].api.accounts
     entities = []
     for account in accounts:
-        if (config_entry.options.get("accounts", None) is None) or (
-            account.user_id in config_entry.options.get("accounts", [])) or (
-            len(config_entry.options.get("accounts", []))==0
+        if (
+            (config_entry.options.get("accounts", None) is None)
+            or (account.user_id in config_entry.options.get("accounts", []))
+            or (len(config_entry.options.get("accounts", [])) == 0)
         ):
             for platform in DEFAULT_OVERRIDE_ENTITIES:
                 entities.append(
@@ -40,7 +39,7 @@ async def async_setup_entry(
                         coordinator=hass.data[DOMAIN][config_entry.entry_id],
                         idx=None,
                         account_id=account.user_id,
-                        platform=platform
+                        platform=platform,
                     )
                 )
             for app in config_entry.options.get("tracked_applications", []):
@@ -49,11 +48,12 @@ async def async_setup_entry(
                         coordinator=hass.data[DOMAIN][config_entry.entry_id],
                         idx=None,
                         account_id=account.user_id,
-                        app_id=app
+                        app_id=app,
                     )
                 )
 
     async_add_entities(entities, True)
+
 
 class ApplicationBlockSwitch(ApplicationEntity, SwitchEntity):
     """Define application switch."""
@@ -61,7 +61,7 @@ class ApplicationBlockSwitch(ApplicationEntity, SwitchEntity):
     @property
     def name(self) -> str:
         """Return entity name."""
-        return f"Block {self._application.name}"
+        return f"{self._account.first_name} Block {self._application.name}"
 
     @property
     def is_on(self) -> bool:
@@ -83,21 +83,20 @@ class ApplicationBlockSwitch(ApplicationEntity, SwitchEntity):
         await self._application.block_app()
         await self.coordinator.async_request_refresh()
 
+
 class PlatformOverrideSwitch(PlatformOverrideEntity, SwitchEntity):
     """Platform override switch."""
 
-    def __init__(self,
-                 coordinator: FamilySafetyCoordinator,
-                 idx,
-                 account_id,
-                 platform) -> None:
+    def __init__(
+        self, coordinator: FamilySafetyCoordinator, idx, account_id, platform
+    ) -> None:
         """Create PlatformOverrideSwitch."""
         super().__init__(coordinator, idx, account_id, platform)
 
     @property
     def name(self) -> str:
         """Return entity name."""
-        return f"Block {str(self._platform)}"
+        return f"{self._account.first_name} Block {str(self._platform)}"
 
     @property
     def is_on(self) -> bool:
